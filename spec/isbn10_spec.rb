18248e423450d8a324d54eed824bec522a7e99b2
James Healy <james@yob.id.au>
2017-07-15 22:48:55 +1000

    update specs to rspec 3.0 expect syntax
    no more monkey patching

--- spec/isbn10_spec.rb
+++ spec/isbn10_spec.rb
@@ -1,37 +1,34 @@
-$LOAD_PATH << File.dirname(__FILE__) + "/../lib"
-
-require 'spec'
 require 'isbn10'
 
 describe ISBN10 do
   it "should identify a valid ISBN10" do
-    ISBN10.new("0140449043").valid?.should be_true
-    ISBN10.new("0-140-44904-3").valid?.should be_true
+    expect(ISBN10.new("0140449043").valid?).to be_truthy
+    expect(ISBN10.new("0-140-44904-3").valid?).to be_truthy
   end
 
   it "should identify a valid ISBN10" do
-    ISBN10.valid?("0140449043").should be_true
-    ISBN10.valid?("0-140-44904-3").should be_true
+    expect(ISBN10.valid?("0140449043")).to be_truthy
+    expect(ISBN10.valid?("0-140-44904-3")).to be_truthy
   end
   
   it "should identify an invalid ISBN10" do
-    ISBN10.valid?(nil).should              be_false
-    ISBN10.valid?("0-1404-4904-2").should   be_false
-    ISBN10.valid?("0140449042").should   be_false
-    ISBN10.valid?("978184354916").should   be_false
-    ISBN10.valid?(Array).should            be_false
-    ISBN10.valid?(Array.new).should        be_false
+    expect(ISBN10.valid?(nil)).to              be_falsey
+    expect(ISBN10.valid?("0-1404-4904-2")).to   be_falsey
+    expect(ISBN10.valid?("0140449042")).to   be_falsey
+    expect(ISBN10.valid?("978184354916")).to   be_falsey
+    expect(ISBN10.valid?(Array)).to            be_falsey
+    expect(ISBN10.valid?(Array.new)).to        be_falsey
   end
 
   it "should calculate a ISBN10 check digit correctly" do
-    ISBN10.complete("014044904").should eql("0140449043")
+    expect(ISBN10.complete("014044904")).to eql("0140449043")
   end
 
   it "should convert to an EAN correctly" do
-    ISBN10.new("0140449042").to_ean.should be_nil
+    expect(ISBN10.new("0140449042").to_ean).to be_nil
     isbn = ISBN10.new("0140449043")
-    isbn.valid?.should be_true
-    isbn.to_ean.should eql("9780140449044")
+    expect(isbn.valid?).to be_truthy
+    expect(isbn.to_ean).to eql("9780140449044")
   end
 
 end
